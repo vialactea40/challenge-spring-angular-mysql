271266a49c7d4b47a45e225c4d0d01840493df56
PauloV <paulo_victor_40@hotmail.com>
2023-04-12 00:06:35 -0300

    Iniciando Department e indo pra uma nova branch

--- back/src/main/java/com/paulo/back/controller/DepartmentController.java
+++ back/src/main/java/com/paulo/back/controller/DepartmentController.java
@@ -16,50 +16,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.paulo.back.model.Enterprise;
-import com.paulo.back.repository.EnterpriseRepository;
+import com.paulo.back.model.Department;
+import com.paulo.back.repository.DepartmentRepository;
 
 @RestController
-@RequestMapping("/api/enterprises")
-public class EnterpriseController {
+@RequestMapping("/api/departments")
+public class DepartmentController {
 
     @Autowired
-    private EnterpriseRepository enterpriseRepository;
+    private DepartmentRepository departmentRepository;
 
-    public EnterpriseController(EnterpriseRepository enterpriseRepository) {
-        this.enterpriseRepository = enterpriseRepository;
+    public DepartmentController(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
     }
 
     @GetMapping
-    public List<Enterprise> list() {
-        return enterpriseRepository.findAll();
+    public List<Department> list() {
+        return departmentRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Enterprise> findById(@PathVariable Long id) {
-        return enterpriseRepository.findById(id)
+    public ResponseEntity<Department> findById(@PathVariable Long id) {
+        return departmentRepository.findById(id)
             .map(recordFound -> ResponseEntity.ok().body(recordFound))
             .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Enterprise create(@RequestBody Enterprise enterprise) {
-        return enterpriseRepository.save(enterprise);
+    public Department create(@RequestBody Department department) {
+        return departmentRepository.save(department);
     }
     
     @PutMapping("/{id}")
-    public ResponseEntity<Enterprise> update(@PathVariable Long id, @RequestBody Enterprise enterprise) {
-        return enterpriseRepository.findById(id)
+    public ResponseEntity<Department> update(@PathVariable Long id, @RequestBody Department department) {
+        return departmentRepository.findById(id)
             .map(recordFound -> {
                 recordFound.setModifiedBy("PersonWhoModifiedTODO");
                 recordFound.setModifiedDate(LocalDateTime.now());
-                recordFound.setStatus(enterprise.getStatus());
-                recordFound.setName(enterprise.getName());
-                recordFound.setAddress(enterprise.getAddress());
-                recordFound.setPhone(enterprise.getPhone());
-                Enterprise updated = enterpriseRepository.save(recordFound);
-                return ResponseEntity.ok().body(updated);              
+                recordFound.setDescription(department.getDescription());
+                recordFound.setName(department.getName());
+                recordFound.setPhone(department.getPhone());
+                recordFound.setStatus(department.getStatus());
+                recordFound.setEnterprise(department.getEnterprise());
+                Department updated = departmentRepository.save(recordFound);
+                return ResponseEntity.ok().body(updated);
             })
             .orElse(ResponseEntity.notFound().build());
     }
@@ -67,9 +68,9 @@ public class EnterpriseController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
 
-        return enterpriseRepository.findById(id)
+        return departmentRepository.findById(id)
             .map(recordFound -> {
-                enterpriseRepository.deleteById(id);
+                departmentRepository.deleteById(id);
                 return ResponseEntity.noContent().<Void>build();              
             })
             .orElse(ResponseEntity.notFound().build());
