766f5fee40cd6418a1e95fd7d0dd1c6596cab0b6
PauloV <paulo_victor_40@hotmail.com>
2023-04-12 14:50:26 -0300

    changing to .war instead of .jar

--- back/src/main/java/com/paulo/back/BackApplication.java
+++ back/src/main/java/com/paulo/back/BackApplication.java
@@ -2,9 +2,10 @@ package com.paulo.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackApplication {
+public class BackApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
